4cf4ac74b6395eb394bd08bc8905fd9cae2c6e24
sophierichards <richas3@rpi.edu>
2020-01-31 21:54:20 -0500

    Added a jump

--- GGJ2020/Assets/Scripts/PlayerController.cs
+++ GGJ2020/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 10f;
+    [SerializeField] float jumpPower = 50f;
+    [SerializeField] float gravity = 100f;
     CharacterController cc;
     Vector3 movementDirection;
     // Start is called before the first frame update
@@ -17,17 +20,30 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         movementDirection = HandleInput();
-        MovePlayer(movementDirection*speed);
+        MovePlayer(movementDirection);
     }
+
 
     Vector3 HandleInput(){
         Vector3 direction = Vector3.zero;
         direction.x = Input.GetAxisRaw("Horizontal");
         direction.z = Input.GetAxisRaw("Vertical");
+        //scales movement to camera direction
         direction = Camera.main.transform.TransformDirection(direction);
         direction.Normalize();
-        direction.y = -1;
+        direction = direction*speed;
+        direction.y = GetYAxis();
         return direction;
+    }
+
+
+    //current jumping is too variable, should prob be changed later oops
+    float GetYAxis(){
+        float y = -gravity*Time.deltaTime;
+        if(cc.isGrounded && Input.GetAxisRaw("Jump")==1){
+            y += jumpPower;
+        }
+        return y;
     }
 
     public void MovePlayer(Vector3 velocity){
