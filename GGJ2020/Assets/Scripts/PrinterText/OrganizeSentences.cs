ebb3073575b7e7df9acae1297e84575952e8a335
sophierichards <richas3@rpi.edu>
2020-02-01 10:28:06 -0500

    changed ReadFromCSV to a static function

--- GGJ2020/Assets/Scripts/PrinterText/OrganizeSentences.cs
+++ GGJ2020/Assets/Scripts/PrinterText/OrganizeSentences.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class OrganizeSentences : MonoBehaviour
 {
     List<List<string>> sentences;
-    [SerializeField] ReadTextFile reader;
+    [SerializeField] TextAsset textFile;
     // Start is called before the first frame update
     void Start()
     {
-        sentences = new List<List<string>>();
-        ReadFromCSV();
+        sentences = ReadFromCSV(textFile);
     }
 
     // Update is called once per frame
@@ -19,19 +18,30 @@ public class OrganizeSentences : MonoBehaviour
         
     }
 
-    void ReadFromCSV(){
-        string original = reader.GetString();
+    public static List<List<string>> ReadFromCSV(TextAsset text){
+        List<List<string>> resultSentences = new List<List<string>>();
+        string original = GetString(text);
         string[] groups = original.Split('\n');
         int counter = 0;
         foreach(string group in groups){
             char[] removeChars = {','};
-            string[] sentence = group.Split(removeChars);
-            sentences.Add(new List<string>());
+            //gets rid of extra white space
+            string edittedGroup = group.Trim();
+            string[] sentence = edittedGroup.Split(removeChars);
+            resultSentences.Add(new List<string>());
             foreach (string sent in sentence)
             {
-                sentences[counter].Add(sent.Substring(1,sent.Length-2));
-                Debug.Log(sent.Substring(1,sent.Length-2));
+                resultSentences[counter].Add(sent.Substring(1,sent.Length-2));
             }
         }
+        return resultSentences;
+    }
+
+    public static string GetString(TextAsset text){
+        return text.text;
+    }
+
+    public void LoadTextFile(TextAsset file){
+        textFile = file;
     }
 }
