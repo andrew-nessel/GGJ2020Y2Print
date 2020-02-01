6e128d5b075ee8fff02143d793eaaceef230000f
Andrew Nessel <andrew.c.nessel@gmail.com>
2020-02-01 11:43:04 -0500

    Added the repair action for the player

---
+++ GGJ2020/Assets/Scripts/InteractCollider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractCollider : MonoBehaviour
+{
+
+    public float playerRepairAmount;
+    public float playerRepairCooldown;
+    public float currentCooldown = 0f;
+
+
+    private void OnTriggerStay(Collider other){
+        GameObject otherGO = other.gameObject;
+        if(string.Equals(otherGO.tag, "Repairable")){
+            if(Input.GetButton("Repair")){
+                if(currentCooldown <= 0f){
+                    RepairObject repairO = otherGO.GetComponent<RepairObject>();
+                    repairO.sendRepairs(playerRepairAmount);
+                    currentCooldown = playerRepairCooldown;
+                }else{
+                    currentCooldown -= Time.deltaTime;
+                }
+            }
+        }
+    }
+}
