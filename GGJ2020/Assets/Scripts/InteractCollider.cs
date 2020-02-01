303f0231f60f1e77eee803163965a4e434e1e077
Andrew Nessel <andrew.c.nessel@gmail.com>
2020-02-01 14:49:43 -0500

    Pickup and putdown mechanics

--- GGJ2020/Assets/Scripts/InteractCollider.cs
+++ GGJ2020/Assets/Scripts/InteractCollider.cs
@@ -7,20 +7,85 @@ public class InteractCollider : MonoBehaviour
 
     public float playerRepairAmount;
     public float playerRepairCooldown;
-    public float currentCooldown = 0f;
+    public float playerPickupCooldown;
+    public float currentRepairCooldown = 0f;
+    public float currentPickupCooldown = 0f;
+    public GameObject pickupLocation;
+    public bool isHolding;
+    public GameObject heldObject;
+    public GameObject snapLocation;
+
+    void Update(){
+        if(isHolding){
+            if(Input.GetButton("Pickup") && (currentPickupCooldown <= 0f)){
+                if(snapLocation == null){
+                    heldObject.transform.position = pickupLocation.transform.position + Vector3.forward;
+                    heldObject.transform.parent = null;
+                    heldObject.GetComponent<Rigidbody>().isKinematic = false;
+                    heldObject = null;
+                    isHolding = false;
+                    currentPickupCooldown = playerPickupCooldown;
+                }else{
+                    heldObject.transform.position = snapLocation.transform.position;
+                    heldObject.transform.parent = null;
+                    heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                    heldObject = null;
+                    isHolding = false;
+                    currentPickupCooldown = playerPickupCooldown;
+                }
+            }
+        }
+        if(currentRepairCooldown > 0f){
+            currentRepairCooldown -= Time.deltaTime;
+        }
+        
+        if(currentPickupCooldown > 0f){
+            currentPickupCooldown -= Time.deltaTime;
+        }
+
+    }
 
 
     private void OnTriggerStay(Collider other){
         GameObject otherGO = other.gameObject;
         if(string.Equals(otherGO.tag, "Repairable")){
-            if(Input.GetButton("Repair")){
-                if(currentCooldown <= 0f){
+            if(Input.GetButton("Repair") && (!isHolding)){
+                if(currentPickupCooldown <= 0f){
                     RepairObject repairO = otherGO.GetComponent<RepairObject>();
                     repairO.sendRepairs(playerRepairAmount);
-                    currentCooldown = playerRepairCooldown;
-                }else{
-                    currentCooldown -= Time.deltaTime;
+                    currentPickupCooldown = playerRepairCooldown;
                 }
+            }
+        }
+        if(string.Equals(otherGO.tag, "Pickupable")){
+            if(Input.GetButton("Pickup") && (!isHolding) && (currentPickupCooldown <= 0f)){
+                otherGO.transform.position = pickupLocation.transform.position;
+                otherGO.transform.parent = pickupLocation.transform;
+                heldObject = otherGO;
+                heldObject.GetComponent<Rigidbody>().isKinematic = true;
+                isHolding = true;
+                currentPickupCooldown = playerPickupCooldown;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other){
+        GameObject otherGO = other.gameObject;
+        if(string.Equals(otherGO.tag, "PlaceLocation")){
+            if(isHolding){
+                otherGO.GetComponent<MeshRenderer>().enabled = true;
+                snapLocation = otherGO;
+            }
+        }
+    }
+
+
+    private void OnTriggerExit(Collider other){
+        GameObject otherGO = other.gameObject;
+        if(string.Equals(otherGO.tag, "PlaceLocation")){
+            if(isHolding){
+                otherGO.GetComponent<MeshRenderer>().enabled = false;
+                snapLocation = null;
             }
         }
     }
