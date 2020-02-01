3080e7d536e737ffdce268a82583c6287ce7030c
sophierichards <richas3@rpi.edu>
2020-02-01 00:16:04 -0500

    added csv reader

---
+++ GGJ2020/Assets/Scripts/PrinterText/OrganizeSentences.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganizeSentences : MonoBehaviour
+{
+    List<List<string>> sentences;
+    [SerializeField] ReadTextFile reader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new List<List<string>>();
+        ReadFromCSV();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ReadFromCSV(){
+        string original = reader.GetString();
+        string[] groups = original.Split('\n');
+        int counter = 0;
+        foreach(string group in groups){
+            char[] removeChars = {','};
+            string[] sentence = group.Split(removeChars);
+            sentences.Add(new List<string>());
+            foreach (string sent in sentence)
+            {
+                sentences[counter].Add(sent.Substring(1,sent.Length-2));
+                Debug.Log(sent.Substring(1,sent.Length-2));
+            }
+        }
+    }
+}
