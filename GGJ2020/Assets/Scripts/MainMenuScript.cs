fbe48cac941c58a51a706b488b8fb4168214cf4e
Andrew Nessel <andrew.c.nessel@gmail.com>
2020-02-02 13:16:22 -0500

    Added menu music and title image

--- GGJ2020/Assets/Scripts/MainMenuScript.cs
+++ GGJ2020/Assets/Scripts/MainMenuScript.cs
@@ -5,15 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
+    public AudioSource menuMusic;
+    public AudioClip music1;
+    public AudioClip music2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        menuMusic.clip = music1;
+        menuMusic.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!menuMusic.isPlaying)
+        {
+            menuMusic.clip = music2;
+            menuMusic.Play();
+            menuMusic.loop = true;
+        }
         
     }
 
