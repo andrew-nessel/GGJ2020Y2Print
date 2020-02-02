bf80959ba290c09bb0ac004e6490ea046648786d
Andrew Nessel <andrew.c.nessel@gmail.com>
2020-02-02 15:00:55 -0500

    Added how to and fixed screen scaling

--- GGJ2020/Assets/Scripts/MainMenuScript.cs
+++ GGJ2020/Assets/Scripts/MainMenuScript.cs
@@ -8,10 +8,15 @@ public class MainMenuScript : MonoBehaviour
     public AudioSource menuMusic;
     public AudioClip music1;
     public AudioClip music2;
+    public GameObject mainMenuPanel;
+    public GameObject howToPanel;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        howToPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
         menuMusic.clip = music1;
         menuMusic.Play();
     }
@@ -34,5 +39,10 @@ public class MainMenuScript : MonoBehaviour
 
     public void startGame(){
         SceneManager.LoadScene("PrinterLevel");
+    }
+
+    public void showHowToPlay(){
+        howToPanel.SetActive(true);
+        mainMenuPanel.SetActive(false);
     }
 }
